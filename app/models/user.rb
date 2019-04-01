44db6697738ee6e7ece3526229b5e627c97d473d
tobibako45 <tobibako45@gmail.com>
2019-04-01 09:39:48 +0900

    Fix name required

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :todos, dependent: :destroy
+  validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
 end
