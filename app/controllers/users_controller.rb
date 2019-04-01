a850c1fbe338ba01b3cf732855c6e4c7d4c563d9
tobibako45 <tobibako45@gmail.com>
2019-04-01 10:29:14 +0900

    Fix get only name and description

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.where(id: params[:id]).pluck(:name, :description)
     render json: @user
   end
 
